ccaadf9048ed72c8ce2e2d4ce4970d1d50dd2d8d
Zholud <zholud.zher@mail.ru>
2025-09-08 01:24:44 +0300

    add screen as constant array

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 #define SCREEN_WIDTH 120
 #define SCREEM_HEIGHT 30
 
-void fillScreen(char * screen, char filling_symbol) {
+char screen[SCREEN_WIDTH * SCREEM_HEIGHT + 1];
+
+void fillScreen(char filling_symbol) {
     for (size_t y = 0; y < SCREEM_HEIGHT; y++) {
         for (size_t x = 0; x < SCREEN_WIDTH; x++) {
             screen[x + y * SCREEN_WIDTH] = filling_symbol;
@@ -11,28 +13,23 @@ void fillScreen(char * screen, char filling_symbol) {
     }
 }
 
-void updateScreen(char * screen) {
+void updateScreen() {
     printf("%s", screen);
     //char matrix[3][3] = { {'1', '1', '\n'}, {'2', '2', '\n'}, {'3', '3', '\0'}}; //Thing about it
     //printf("%s", matrix);
 }
 
-void drawDot(char * screen, int x, int y) {
+void drawDot(int x, int y) {
     screen[x + y * SCREEN_WIDTH] = '@';
 }
 
 int main()
 {
-    char screen[SCREEN_WIDTH * SCREEM_HEIGHT + 1];
     screen[SCREEN_WIDTH * SCREEM_HEIGHT] = '\0';
     while (1)
     {
-        fillScreen(screen, ' ');
-        drawDot(screen, 0, 0);
-        drawDot(screen, 119, 0);
-        drawDot(screen, 0, 29);
-        drawDot(screen, 119, 29);
-        updateScreen(screen);
+        fillScreen(' ');
+        updateScreen();
     }
     return 0;
 }
