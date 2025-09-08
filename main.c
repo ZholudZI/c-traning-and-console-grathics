07069e11fc8a63fa2958799227f796588b300fef
Zholud <zholud.zher@mail.ru>
2025-09-09 00:07:39 +0300

    add max() and min() functions

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
-//Fuck Microsoft compiler
+//Fucking Microsoft compiler
 #define SCREEN_WIDTH 120
 #define SCREEM_HEIGHT 30
 
 char screen[SCREEN_WIDTH * SCREEM_HEIGHT + 1];
+
+int min(num1, num2) { //ToDo: analysing bitwise realization
+    if (num1 < num2) {
+        return num1;
+    }
+    else {
+        return num2;
+    }
+}
+
+int max(num1, num2) { //ToDo: analysing bitwise realization
+    if (num1 > num2) {
+        return num1;
+    }
+    else {
+        return num2;
+    }
+}
 
 void fillScreen(char filling_symbol) {
     for (size_t y = 0; y < SCREEM_HEIGHT; y++) {
@@ -23,12 +41,20 @@ void drawDot(int x, int y) {
     screen[x + y * SCREEN_WIDTH] = '@';
 }
 
+void drawLine(int x1, int y1, int x2, int y2) {
+    drawDot(x1, y1);
+    /*for (int x = min(first_x, second_x))*/
+    //printf(max(first_x, second_x));
+    drawDot(x2, y2);
+}
+
 int main()
 {
     screen[SCREEN_WIDTH * SCREEM_HEIGHT] = '\0';
     while (1)
     {
         fillScreen(' ');
+        //drawLine(3, 3, 10, 10);
         updateScreen();
     }
     return 0;
